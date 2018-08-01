9f9b4a984300d99fbcb3e196394430c80b3b6ef1
sergkul <mail@sergk.ru>
2018-08-02 01:22:16 +0300

    'Brain even' game added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+export const greeting = () => {
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
 };
 
-export default greeting;
+export const startGame = () => {
+  let gameOver = false;
+  let attepmt = 1;
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  const makeNumber = () => Math.floor(Math.random() * 100 + 1);
+
+  const checkNumber = n => (n % 2 === 0 ? 'yes' : 'no');
+
+  const checkAnswer = (ans, num) => {
+    const trueAns = checkNumber(num);
+
+    if ((ans !== 'yes' && ans !== 'no') || ans !== trueAns) {
+      console.log(`${ans} is wrong answer ;(. Correct answer was ${trueAns}.\nLet's try again, ${name}!`);
+      gameOver = true;
+    } else {
+      console.log('Correct');
+      attepmt += 1;
+    }
+  };
+
+  const askQuestion = () => {
+    const number = makeNumber();
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    checkAnswer(answer, number);
+  };
+
+  while (!gameOver && attepmt <= 3) {
+    askQuestion();
+  }
+
+  if (attepmt === 4) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
